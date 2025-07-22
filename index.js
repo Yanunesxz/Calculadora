272934d79ad152db695403f9a476d4e3204b6844
Yan Nunes <yanunesxz@gmail.com>
2025-07-22 11:22:01 -0300

    Branch 2

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ console.log(a === b)
 console.log(a == b)
 console.log(a !== b)
 
+//= vira atribuição de valor
 //=== valida não so o conteudo mas tambem o tipo de dado
-//== fala aepnas o conteudo
-//!== fala se é diferente ou não
+//== fala apenas o conteudo mas não tipo de dado
+//!== fala se é diferente 
+
+console.log(a != b && typeof a == 'String')
+
+//&& operador logico, exemplo perguntar se a é maior que b sendo as duas condiçoes verdadeiras ao mesmo tempo usado para comparar os dados
+
+//( typeof a == 'String' ) isso vai perguntar se as duas afirmaçoes são verdadeiras ou não 
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+
+
+let cor = "verde"
+
+if (cor === 'verde') {
+    console.log('siga')
+                     }
+ else('Pare')
+
+ //if/else significa se ou senão dando o exemplo do semafaro para interpretar melhor
+ 
+
