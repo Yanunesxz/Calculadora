46e4cbfcc0b6dac6d834a210819910ef6d64c56d
Yan Nunes <yanunesxz@gmail.com>
2025-07-22 11:05:07 -0300

    Commit inciais

---
+++ index.js
@@ -0,0 +1,10 @@
+let a = 10
+const b = "10"
+
+console.log(a === b)
+console.log(a == b)
+console.log(a !== b)
+
+//=== valida não so o conteudo mas tambem o tipo de dado
+//== fala
+//!== fala se é diferente
