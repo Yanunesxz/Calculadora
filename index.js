0beae15c908cbe3190c3d4dc45af8e21d805c215
Yan Nunes <yanunesxz@gmail.com>
2025-07-22 11:06:59 -0300

    commit 2

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ console.log(a == b)
 console.log(a !== b)
 
 //=== valida não so o conteudo mas tambem o tipo de dado
-//== fala
-//!== fala se é diferente
+//== fala aepnas o conteudo
+//!== fala se é diferente ou não
